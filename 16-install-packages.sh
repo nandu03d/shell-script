774e6cc186574647b59573bbfdd8ed57ce35197f
nandu03d <nandu03d@gmail.com>
2024-06-26 00:26:49 +0530

    commit-message

--- 16-install-packages.sh
+++ 16-install-packages.sh
@@ -5,6 +5,7 @@ scriptName=$(echo $0 | cut -d "." -f1)
 logFile=/tmp/$scriptName-$timeStamp.log
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 if [ $userid -ne 0 ]
 then
@@ -17,18 +18,22 @@ fi
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2...$R FAILURE $N"
+        echo -e "$2...$R FAILURE"
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N"
+        echo -e "$2...$G SUCCESS"
     fi
 }
 
-dnf install mysql -y &>>$logFile
-validate $? "installing mysql"
-
-dnf install git -y &>>$logFile
-validate $? "installing git"
-
-dnf install dockerrr -y &>>$logFile
-validate $? "installing docker"
+for i in $@
+do
+    echo "package to install:$i"
+    dnf list installed $i &>>$logFile
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$logFile
+        validate $? "instalation of $i"
+    fi
+done
