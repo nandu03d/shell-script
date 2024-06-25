d4396852d3009bc6206da286c69d973c468872bb
nandu03d <nandu03d@gmail.com>
2024-06-25 22:47:41 +0530

    commit-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-# userid=$(id -u)
-# if [ $userid -ne 0 ]
-# then
-#     echo "please run this script with root access"
-# else
-#     echo "you are super user"
-# fi
-# dnf install mysql -y
-# if [ $? -ne 0 ]
-# then
-#     echo "installation of mysql is FAILURE"
-#     exit 1
-# else
-#     echo "installation of mysql is SUCCESS"
-# fi
-
-
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
+userid=$(id -u)
+if [ $userid -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    echo "please run this script with root access"
 else
-    echo "You are super user."
+    echo "you are super user"
 fi
-
 dnf install mysql -y
-
 if [ $? -ne 0 ]
 then
-    echo "Installation of mysql...FAILURE"
+    echo "installation of mysql is FAILURE"
     exit 1
 else
-    echo "Installation of mysql...SUCCESS"
+    echo "installation of mysql is SUCCESS"
 fi
+
+
+
+# USERID=$(id -u)
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1 # manually exit if error comes.
+# else
+#     echo "You are super user."
+# fi
+
+# dnf install mysql -y
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installation of mysql...FAILURE"
+#     exit 1
+# else
+#     echo "Installation of mysql...SUCCESS"
+# fi
