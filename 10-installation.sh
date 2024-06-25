468bc349987f6354d98d6406063737d7c0cc06b9
nandu03d <nandu03d@gmail.com>
2024-06-25 22:36:12 +0530

    commit-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# userid=$(id -u)
-# if [ $userid -ne 0 ]
-# then
-#     echo "please run this script with root access"
-# else
-#     echo "you are super user"
-# fi
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "please run this script with root access"
+else
+    echo "you are super user"
+fi
 dnf install mysql -y
