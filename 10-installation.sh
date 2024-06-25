b69c783deab532f8aa3db50831101b85eb33bd4c
nandu03d <nandu03d@gmail.com>
2024-06-25 22:39:56 +0530

    commit-message

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,3 +7,10 @@ else
     echo "you are super user"
 fi
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql is FAILURE"
+    exit 1
+else
+    echo "installation of mysql is SUCCESS"
+fi
