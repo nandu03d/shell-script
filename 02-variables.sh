ca67ad88930ea0e39f55aaa036d048f5ac9ac5c2
nandu03d <nandu03d@gmail.com>
2024-06-25 18:58:42 +0530

    shell-practice

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-person1=nandu
-person2=abhi
-echo "$person1: hi $person2"
-echo "$person2: hi $person1"
+echo "nandu: hi abhi"
+echo "abhi: hi nandu"
---
+++ 03-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+person1=nandu
+person2=abhi
+echo "$person1: hi $person2"
+echo "$person2: hi $person1"
