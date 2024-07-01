75d56d779012a0ae6bc6b8db607bccf884e6f952
nandu03d <nandu03d@gmail.com>
2024-07-01 15:58:19 +0530

    commit-message

--- 22-echo.sh
+++ 22-echo.sh
@@ -4,4 +4,5 @@ scriptName=$(echo $0 | cut -d "." -f1)
 logFile=/tmp/$scriptName-$timeStamp.log
 echo -e "\e[32m hi" 1>>$logFile
 ech -e "\e[31m hello" 2>>$logFile
+exit 1
 echo -e "\e[32m bye" 1>>$logFile
