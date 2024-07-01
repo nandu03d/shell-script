f844581104c6977bf814c7bd1e889fb5263ea7c2
nandu03d <nandu03d@gmail.com>
2024-07-01 17:41:01 +0530

    commit-message

--- 22-echo.sh
+++ 22-echo.sh
@@ -5,21 +5,29 @@ logFile=/tmp/$scriptName-$timeStamp.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-$1=$?
-validate(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-echo -e "$G hi" &>>$logFile
-validate $? "status is"
+# validate(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2...$R FAILURE $N"
+#         exit 1
+#     else
+#         echo -e "$2...$G SUCCESS $N"
+#     fi
+# }
+# echo -e "$G hi" &>>$logFile
+# validate $? "status is"
 
 # ech -e "$R hello" &>>$logFile
 # validate $? "status is"
+
+echo "hi" &>>$logFile
+if [ $? -ne 0 ]
+then
+    echo -e "$R yes" &>>$logFile
+    exit 1
+else
+    echo -e "$G no" &>>$logFile
+fi
 
 sudo dnf install mysqlll -y
 if [ $? -ne 0 ]
