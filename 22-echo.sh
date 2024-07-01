4db8f04af5d0bbc51b0c4bf1cb99e8fc8edb5f1c
nandu03d <nandu03d@gmail.com>
2024-07-01 16:49:39 +0530

    commit-message

--- 22-echo.sh
+++ 22-echo.sh
@@ -9,7 +9,7 @@ validate(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2...$R FAILURE $N"
-        # exit 1
+        exit 1
     else
         echo -e "$2...$G SUCCESS $N"
     fi
@@ -17,7 +17,7 @@ validate(){
 echo -e "$G hi $N" &>>$logFile
 validate $? "status is"
 
-sudo dnf install mysqlll -y &>>$logFile
+ech -e "$R hello $N" &>>$logFile
 validate $? "status is"
 
 echo -e "$G bye $N" &>>$logFile
