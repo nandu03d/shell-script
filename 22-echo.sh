0c5d9ffe478938f56eae2bca23efd91f152803c6
nandu03d <nandu03d@gmail.com>
2024-07-01 17:58:18 +0530

    commit-message

--- 22-echo.sh
+++ 22-echo.sh
@@ -5,35 +5,35 @@ logFile=/tmp/$scriptName-$timeStamp.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-# validate(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo -e "$2...$R FAILURE $N"
-#         exit 1
-#     else
-#         echo -e "$2...$G SUCCESS $N"
-#     fi
-# }
-# echo -e "$G hi" &>>$logFile
-# validate $? "status is"
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+echo "hi" &>>$logFile
+validate $? "status is"
 
-# ech -e "$R hello" &>>$logFile
-# validate $? "status is"
+ech "bye" &>>$logFile
+validate $? "status is"
 
-echo -e "$G hi $N"
-if [ $? -ne 0 ]
-then
-    echo -e "$R fail" &>>$logFile
-    exit 1
-else
-    echo -e "$G success" &>>$logFile
-fi
+# echo "hi"
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$R fail" &>>$logFile
+#     exit 1
+# else
+#     echo -e "$G success" &>>$logFile
+# fi
 
-ech -e "$R bye $N"
-if [ $? -ne 0 ]
-then
-    echo -e "$R fail" &>>$logFile
-    exit 1
-else
-    echo -e "$G success" &>>$logFile
-fi
+# ech "bye"
+# if [ $? -ne 0 ]
+# then
+#     echo -e "$R fail" &>>$logFile
+#     exit 1
+# else
+#     echo -e "$G success" &>>$logFile
+# fi
